97ac0cf8a5853925c9d0fc4c95f6798f8302526c
zain3x <zain.fla@gmail.com>
2025-06-18 01:50:52 +0700

    add project web-sanlog

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { WebSanlogComponent } from './project/web-sanlog/web-sanlog.component';  // <– create this
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }, // '' = root url
+  { path: '', component: HomeComponent },           // root (intuise.com)
+
+  // ➜ client-preview page (intuise.com/proj/web-sanlog)
+  { path: 'proj/web-sanlog', component: WebSanlogComponent },
+
+  // optional: wildcard fallback
+  { path: '**', redirectTo: '' },
 ];
---
+++ src/app/project/web-sanlog/web-sanlog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-sanlog',
+  imports: [],
+  templateUrl: './web-sanlog.component.html',
+  styleUrl: './web-sanlog.component.scss'
+})
+export class WebSanlogComponent {
+
+}
